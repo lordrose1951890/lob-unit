9e96b948501854c14e12d7b43c64fc1d20b9c30b
LordRose <lordrose1951980@gmail.com>
2021-05-12 16:08:24 +0700

    Refactor testTestMethod pass.

--- src/main/java/org/lordrose/units/WasRun.java
+++ src/main/java/org/lordrose/units/WasRun.java
@@ -1,10 +1,15 @@
 package org.lordrose.units;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
     public boolean wasRun;
+    String testMethod;
 
     public WasRun(String testMethod) {
         wasRun = false;
+        this.testMethod = testMethod;
     }
 
     public void testMethod() {
@@ -12,6 +17,14 @@ public class WasRun {
     }
 
     public void run() {
-        testMethod();
+        Method test;
+        try {
+            test = WasRun.class.getDeclaredMethod(testMethod);
+            test.invoke(this);
+        } catch (InvocationTargetException
+                | NoSuchMethodException
+                | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
